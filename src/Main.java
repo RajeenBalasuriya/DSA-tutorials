28e6999a4b20384f513bb67f1bc0e98cfcb24e81
Rajeen Balasuriya <102136700+RajeenBalasuriya@users.noreply.github.com>
2024-02-19 02:17:17 +0530

    binary search implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        THIS CODE IS FOR TESTING OF BINARY SEARCH
+
+        int[] testArray = new int[12];
+
+        //adding items to array
+        for(int i=0;i<12;i++){
+            testArray[i]=i;
+        }
+
+        binarySearch searchIt = new binarySearch(11,testArray);
+
+        int index=searchIt.binarySearchIt();
+        System.out.println(index);
+
+         */
     }
 }
